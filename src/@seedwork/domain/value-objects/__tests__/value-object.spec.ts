0d01f39eb81d168cedf6bb66a5dbb71e3f1d2a48
Lucas Gois <lucas.gois@linnks.com.br>
2022-03-12 19:00:52 +0000

    test: add value object to string test

--- src/@seedwork/domain/value-objects/__tests__/value-object.spec.ts
+++ src/@seedwork/domain/value-objects/__tests__/value-object.spec.ts
@@ -12,4 +12,48 @@ describe("ValueObject tests", () => {
     const vo = new StubValueObject({ prop1: "value1" });
     expect(vo.value).toStrictEqual({ prop1: "value1" });
   });
+
+  it("Should convert to string", () => {
+    let vo = new StubValueObject({ prop1: "value1" });
+
+    let valueString = `${vo}`;
+
+    expect(valueString).toBe(valueString);
+
+    vo = new StubValueObject(null);
+
+    valueString = `${vo}`;
+
+    expect(valueString).toBe("null");
+
+    vo = new StubValueObject(undefined);
+
+    valueString = `${vo}`;
+
+    expect(valueString).toBe("undefined");
+
+    vo = new StubValueObject(true);
+
+    valueString = `${vo}`;
+
+    expect(valueString).toBe("true");
+
+    vo = new StubValueObject(Number(5));
+
+    valueString = `${vo}`;
+
+    expect(valueString).toBe("5");
+
+    vo = new StubValueObject([]);
+
+    valueString = `${vo}`;
+
+    expect(valueString).toBe("");
+
+    vo = new StubValueObject([1, 2, 3]);
+
+    valueString = `${vo}`;
+
+    expect(valueString).toBe("1,2,3");
+  });
 });
--- src/@seedwork/domain/value-objects/value-object.ts
+++ src/@seedwork/domain/value-objects/value-object.ts
@@ -1,11 +1,27 @@
-export default abstract class ValueObject<Value = any>{
-    protected _value: Value;
+export default abstract class ValueObject<Value = any> {
+  protected _value: Value;
 
-    constructor(value: Value){
-        this._value = value
+  constructor(value: Value) {
+    this._value = value;
+  }
+
+  get value(): Value {
+    return this._value;
+  }
+
+  toString = () => {
+    if (typeof this.value !== "object" || this.value === null) {
+      try {
+        return this.value.toString();
+      } catch (error) {
+        return this.value + "";
+      }
     }
 
-    get value(): Value {
-        return this._value
-    }
+    const valueString = this.value.toString();
+
+    return valueString === "[object Object]"
+      ? JSON.stringify(this.value)
+      : valueString;
+  };
 }
